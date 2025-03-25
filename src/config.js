212f8640b97037d916b6cee0bf55f89e82e065da
Aria Fatah <ariafatah0711@gmail.com>
2025-03-26 05:29:28 +0700

    update: add veresion

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const apiDomain = import.meta.env.VITE_API_DOMAIN;
 const correctPassword = import.meta.env.VITE_NOTES_PASSWORD;
+const versionApp = import.meta.env.VITE_VERSION;
 
-export { apiDomain, correctPassword };
+export { apiDomain, correctPassword, versionApp };
